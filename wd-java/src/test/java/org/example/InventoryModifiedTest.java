5051967de7268b6d10dbb19351ef531c4ad17441
Kerem Beygo <kerem.beygo@saucelabs.com>
2023-07-04 11:40:40 +0200

    Fetch username and accessKey once per test

--- wd-java/src/test/java/org/example/InventoryModifiedTest.java
+++ wd-java/src/test/java/org/example/InventoryModifiedTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import com.saucelabs.visual.Region;
 import com.saucelabs.visual.VisualApi;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.example.pageobjects.InventoryPage;
 import org.example.pageobjects.LoginPage;
 import org.junit.jupiter.api.AfterAll;
@@ -11,17 +12,19 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 
-import static org.example.TestUtils.dotenv;
-
 public class InventoryModifiedTest {
+
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String username = dotenv.get("SAUCE_USERNAME");
+    private static final String accessKey = dotenv.get("SAUCE_ACCESS_KEY");
 
     private static VisualApi visual;
     private static RemoteWebDriver driver;
 
     @BeforeAll
     public static void init() throws MalformedURLException {
-        driver = TestUtils.getWebDriver();
-        visual = new VisualApi(driver, Region.US_WEST_1, dotenv.get("SAUCE_USERNAME"), dotenv.get("SAUCE_ACCESS_KEY"));
+        driver = TestUtils.getWebDriver(username, accessKey);
+        visual = new VisualApi(driver, Region.US_WEST_1, username, accessKey);
     }
 
     @Test
--- wd-java/src/test/java/org/example/TestUtils.java
+++ wd-java/src/test/java/org/example/TestUtils.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -8,19 +7,19 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class TestUtils {
-    public static Dotenv dotenv = Dotenv.load();
-
-    static RemoteWebDriver getWebDriver() throws MalformedURLException {
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setBrowserName("chrome");
-
-        String username = dotenv.get("SAUCE_USERNAME");
-        String accessKey = dotenv.get("SAUCE_ACCESS_KEY");
+    
+    static RemoteWebDriver getWebDriver(String username, String accessKey) throws MalformedURLException {
         if (username == null || accessKey == null || username.trim().isEmpty() || accessKey.trim().isEmpty()) {
             String err = "Sauce Labs credentials not found. Please set SAUCE_USERNAME and SAUCE_ACCESS_KEY in src/test/resources/.env file";
             throw new RuntimeException(err);
         }
+        // Can be found at "Driver creation" on https://app.saucelabs.com/user-settings
         String webDriverUrl = "https://" + username + ":" + accessKey + "@ondemand.saucelabs.com:443/wd/hub";
+
+        // Set capabilities for WebDriver
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setBrowserName("chrome");
+
         return new RemoteWebDriver(new URL(webDriverUrl), caps);
     }
 }
