bdc05074fdc1e699eef0b344dae06501d34cf618
sceiler <sceiler@users.noreply.github.com>
2023-07-06 11:40:12 +0200

    Add visual example using TestNG

---
+++ wd-java-testng/src/test/java/org/example/InventoryModifiedTest.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import com.saucelabs.visual.Region;
+import com.saucelabs.visual.VisualApi;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.example.pageobjects.InventoryPage;
+import org.example.pageobjects.LoginPage;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+
+public class InventoryModifiedTest {
+
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String username = dotenv.get("SAUCE_USERNAME");
+    private static final String accessKey = dotenv.get("SAUCE_ACCESS_KEY");
+
+    private static VisualApi visual;
+    private static RemoteWebDriver driver;
+
+    @BeforeSuite
+    public static void init() throws MalformedURLException {
+        driver = TestUtils.getWebDriver(username, accessKey);
+        visual = new VisualApi(driver, Region.US_WEST_1, username, accessKey);
+    }
+
+    @Test
+    void checkInventoryPageLooksTheSame() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+
+        visual.check("Before Login");
+
+        loginPage.login("standard_user", "secret_sauce");
+
+        InventoryPage inventoryPage = new InventoryPage(driver);
+        inventoryPage.open();
+        inventoryPage.addBackpackToCart();
+
+        visual.check("Inventory page");
+    }
+
+    @AfterSuite
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
---
+++ wd-java-testng/src/test/java/org/example/pageobjects/InventoryPage.java
@@ -0,0 +1,26 @@
+package org.example.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class InventoryPage {
+
+    private final WebDriver driver;
+
+    public InventoryPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void open() {
+        driver.get("https://www.saucedemo.com/inventory.html");
+    }
+
+    public WebElement getAddBackpackToCartButton() {
+        return driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
+    }
+
+    public void addBackpackToCart() {
+        getAddBackpackToCartButton().click();
+    }
+}
