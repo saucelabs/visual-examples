90494fe65df17ecfb308c6195cb106711f8180bc
Pawel Tomaszewski <pawel.tomaszewski@saucelabs.com>
2023-10-18 12:48:15 +0200

    fix setWindowRect

--- wdio/src/configs/wdio.saucelabs.desktop.conf.ts
+++ wdio/src/configs/wdio.saucelabs.desktop.conf.ts
@@ -52,7 +52,6 @@ export const config: Options.Testrunner = {
   // =====
   before: async (_capabilities, _specs) => {
     // Set all browsers to the "same" viewport
-    // @ts-ignore
-    await browser.setWindowRect(1920, 1080);
+    await browser.setWindowRect(null, null, 1920, 1080);
   },
 };
