2256bec003ef7f466f951d7f93870609573e8c9f
Pawel Tomaszewski <pawel.tomaszewski@saucelabs.com>
2023-10-18 12:40:48 +0200

    comments

--- wdio/src/configs/wdio.saucelabs.mobile.conf.ts
+++ wdio/src/configs/wdio.saucelabs.mobile.conf.ts
@@ -3,59 +3,6 @@ import { config as sauceSharedConfig } from './wdio.saucelabs.shared.conf.ts';
 
 const build = `Sauce Demo Test - ${new Date().getTime()}`;
 
-const iosCapabilitiesShared = {
-  'appium:automationName': 'XCUITest',
-  browserName: 'Safari',
-  platformName: 'iOS',
-  'sauce:options': {
-    build,
-  },
-}
-const iosCapabilities = ['15.5', '16.2'].map((iOSVersion) => ({
-  ...iosCapabilitiesShared,
-  'appium:platformVersion': iOSVersion,
-  'appium:deviceName': 'iPhone Simulator',
-  'sauce:options': {
-    build,
-    ...(Math.floor(+iOSVersion) >= 16 ? { appiumVersion: '2.0.0' } : {}),
-  },
-}));
-const iosRealCapabilities = ['15'].map((iOSVersion) => ({
-  ...iosCapabilitiesShared,
-  'appium:platformVersion': iOSVersion,
-  'appium:deviceName': 'iPhone X',
-  'sauce:options': {
-    appiumVersion: '2.0.0',
-    build,
-  },
-}));
-
-const androidCapabilitiesShared = {
-  'appium:automationName': 'UiAutomator2',
-  browserName: 'Chrome',
-  platformName: 'Android',
-  'sauce:options': {
-    build
-  },
-}
-const androidCapabilities = ['13.0', '14.0'].map(
-  (androidVersion) => ({
-    ...androidCapabilitiesShared,
-    'appium:platformVersion': androidVersion,
-    'appium:deviceName': 'Android GoogleAPI Emulator',
-  })
-);
-const androidRealCapabilities = ['13'].map(
-  (androidVersion) => ({
-    ...androidCapabilitiesShared,
-    'appium:platformVersion': androidVersion,
-    'appium:deviceName': 'Samsung Galaxy S22',
-    'sauce:options': {
-      build,
-    }
-  })
-);
-
 export const config: Options.Testrunner = {
   ...sauceSharedConfig,
   // ============
@@ -66,5 +13,48 @@ export const config: Options.Testrunner = {
   // and http://appium.io/docs/en/2.0/guides/caps/
   // For available devices please check
   // https://app.saucelabs.com/live/app-testing
-  capabilities: [ ...iosCapabilities, ...iosRealCapabilities, ...androidCapabilities, ...androidRealCapabilities],
+  capabilities: [
+    {
+      'appium:deviceName': 'iPhone Simulator',
+      'appium:platformVersion': '16.2',
+      'appium:automationName': 'XCUITest',
+      browserName: 'Safari',
+      platformName: 'iOS',
+      'sauce:options': {
+        appiumVersion: '2.0.0',
+        build
+      },
+    },
+    {
+      'appium:deviceName': 'iPhone 11',
+      'appium:platformVersion': '15',
+      'appium:automationName': 'XCUITest',
+      browserName: 'Safari',
+      platformName: 'iOS',
+      'sauce:options': {
+        build,
+      },
+    },
+    {
+      'appium:deviceName': 'Android GoogleAPI Emulator',
+      'appium:platformVersion': '14.0',
+      'appium:automationName': 'UiAutomator2',
+      browserName: 'Chrome',
+      platformName: 'Android',
+      'sauce:options': {
+        build
+      },
+    },
+    {
+      'appium:deviceName': 'Google Pixel 8',
+      'appium:platformVersion': '14',
+      'appium:automationName': 'UiAutomator2',
+      browserName: 'Chrome',
+      platformName: 'Android',
+      'sauce:options': {
+        appiumVersion: '2.0.0',
+        build
+      },
+    }
+  ],
 };
