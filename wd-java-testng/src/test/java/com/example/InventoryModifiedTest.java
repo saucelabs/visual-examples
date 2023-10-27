3e38c971bcbf4a7615bf774da72feddba8be609d
Andreas Lüdeke <andreas.luedeke@saucelabs.com>
2023-10-27 16:18:52 +0200

    update to java 0.3.107 and adopt incompatible changes

--- wd-java-testng/src/test/java/com/example/InventoryModifiedTest.java
+++ wd-java-testng/src/test/java/com/example/InventoryModifiedTest.java
@@ -2,7 +2,7 @@ package com.example;
 
 import com.example.pageobjects.InventoryPage;
 import com.example.pageobjects.LoginPage;
-import com.saucelabs.visual.Region;
+import com.saucelabs.visual.DataCenter;
 import com.saucelabs.visual.VisualApi;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterSuite;
@@ -24,7 +24,7 @@ public class InventoryModifiedTest {
     @BeforeSuite
     public static void init() throws MalformedURLException {
         driver = TestUtils.getDriver(username, accessKey);
-        visual = new VisualApi(driver, Region.US_WEST_1, username, accessKey);
+        visual = new VisualApi(driver, DataCenter.US_WEST_1, username, accessKey);
     }
 
     @Test
--- wd-java/src/test/java/com/example/InventoryTest.java
+++ wd-java/src/test/java/com/example/InventoryTest.java
@@ -2,7 +2,7 @@ package com.example;
 
 import com.example.pageobjects.InventoryPage;
 import com.example.pageobjects.LoginPage;
-import com.saucelabs.visual.Region;
+import com.saucelabs.visual.DataCenter;
 import com.saucelabs.visual.VisualApi;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -24,7 +24,7 @@ public class InventoryTest {
     @BeforeAll
     public static void init() throws MalformedURLException {
         driver = TestUtils.getDriver(username, accessKey);
-        visual = new VisualApi(driver, Region.US_WEST_1, username, accessKey);
+        visual = new VisualApi(driver, DataCenter.US_WEST_1, username, accessKey);
     }
 
     @Test
