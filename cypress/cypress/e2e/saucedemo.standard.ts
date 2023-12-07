c6d61d13bd0a5be9b4d2f0b215c910731265c881
Daniel Graham <daniel.graham@saucelabs.com>
2023-12-07 12:05:55 -0800

    oops

--- cypress/cypress/e2e/saucedemo.standard.ts
+++ cypress/cypress/e2e/saucedemo.standard.ts
@@ -3,6 +3,6 @@ context('Sauce Demo', () => {
     cy.visit('https://www.saucedemo.com/')
     cy.get('[data-test="username"]').type('standard_user').should('have.value', 'standard_user');
     cy.get('[data-test="password"]').type('secret_sauce').should('have.value', 'secret_sauce');
-    cy.sauceVisualCheck('login-page 350p');
+    cy.sauceVisualCheck('login-page');
   })
 });
