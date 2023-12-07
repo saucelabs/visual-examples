0ed5409b9247fd067d7edfbd3dcb514f20e5882e
Daniel Graham <daniel.graham@saucelabs.com>
2023-12-07 12:09:55 -0800

    again

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -4,7 +4,7 @@ const { CypressSauceVisual } = require('@saucelabs/cypress-visual-plugin');
 module.exports = defineConfig({
   e2e: {
     saucelabs: {
-      project: process.env.GITHUB_REPOSITORY || `Cypress Visual Testing Example for ${process.env.SAUCE_USERNAME}!!!`,
+      project: process.env.GITHUB_REPOSITORY || `Cypress Visual Testing Example for ${process.env.SAUCE_USERNAME}`,
       branch: process.env.GITHUB_REF_NAME,
       buildName: `Cypress CI - ${process.env.GITHUB_RUN_NUMBER}`,
       region: 'us-west-1',
