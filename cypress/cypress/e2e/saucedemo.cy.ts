a99cd0109a6947d2d22e7c5b61af1afd330eebe7
Kerem <github@beygo.info>
2024-03-12 17:28:21 +0100

    disable service worker

--- cypress/cypress/e2e/saucedemo.cy.ts
+++ cypress/cypress/e2e/saucedemo.cy.ts
@@ -2,6 +2,7 @@ describe('Sauce Visual Demo', () => {
 
   it(' - should be able to open the login page', () => {
     cy.visit('https://www.saucedemo.com/');
+    cy.intercept('/service-worker.js', {body: undefined})
     cy.get('.login_container').should('be.visible');
     cy.sauceVisualCheck('Before Login', {
       captureDom: true
@@ -10,6 +11,7 @@ describe('Sauce Visual Demo', () => {
 
   it(` - should be able to login with a standard user`, () => {
     cy.visit('https://www.saucedemo.com/');
+    cy.intercept('/service-worker.js', {body: undefined})
     const USER = Cypress.env('VISUAL_CHECK') ? 'visual_user' : 'standard_user';
     cy.get('[data-test="username"]').type(USER).should('have.value', USER);
     cy.get('[data-test="password"]').type('secret_sauce').should('have.value', 'secret_sauce');
@@ -22,6 +24,7 @@ describe('Sauce Visual Demo', () => {
 
   it(' - should not be able to login with a locked user', () => {
     cy.visit('https://www.saucedemo.com/');
+    cy.intercept('/service-worker.js', {body: undefined})
     cy.get('[data-test="username"]').type('locked_out_user').should('have.value', 'locked_out_user');
     cy.get('[data-test="password"]').type('secret_sauce').should('have.value', 'secret_sauce');
     cy.get('input[data-test="login-button"]').click();
