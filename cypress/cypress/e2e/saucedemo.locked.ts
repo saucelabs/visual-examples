7c57e0ba65b4bee5c070050091eb82b3280799b0
Kerem <github@beygo.info>
2024-03-12 11:54:07 +0100

    renaming tests

--- cypress/cypress/e2e/saucedemo.locked.ts
+++
@@ -1,8 +0,0 @@
-context('Sauce Demo', () => {
-  it('.type() - type into a DOM element', () => {
-    cy.visit('https://www.saucedemo.com/')
-    cy.get('[data-test="username"]').type('locked_user').should('have.value', 'locked_user');
-    cy.get('[data-test="password"]').type('secret_sauce').should('have.value', 'secret_sauce');
-    cy.sauceVisualCheck('login-page');
-  })
-});
