4ca1fb8c0da8e0b2386be72edd912cc697369495
Kerem <github@beygo.info>
2024-03-13 11:07:24 +0100

    copy visual over demo

--- cypress/cypress/e2e/saucedemo.cy.ts
+++ cypress/cypress/e2e/saucedemo.cy.ts
@@ -1,8 +1,7 @@
 describe('Sauce Visual Demo', () => {
 
   it(' - should be able to open the login page', () => {
-    cy.visit('https://www.saucedemo.com/');
-    cy.intercept('/service-worker.js', {body: undefined})
+    openSauceDemoPage();
     cy.get('.login_container').should('be.visible');
     cy.sauceVisualCheck('Before Login', {
       captureDom: true
@@ -10,8 +9,7 @@ describe('Sauce Visual Demo', () => {
   })
 
   it(` - should be able to login with a standard user`, () => {
-    cy.visit('https://www.saucedemo.com/');
-    cy.intercept('/service-worker.js', {body: undefined})
+    openSauceDemoPage();
     const USER = Cypress.env('VISUAL_CHECK') ? 'visual_user' : 'standard_user';
     cy.get('[data-test="username"]').type(USER).should('have.value', USER);
     cy.get('[data-test="password"]').type('secret_sauce').should('have.value', 'secret_sauce');
@@ -23,16 +21,21 @@ describe('Sauce Visual Demo', () => {
   })
 
   it(' - should not be able to login with a locked user', () => {
-    cy.visit('https://www.saucedemo.com/');
-    cy.intercept('/service-worker.js', {body: undefined})
+    openSauceDemoPage();
     cy.get('[data-test="username"]').type('locked_out_user').should('have.value', 'locked_out_user');
     cy.get('[data-test="password"]').type('secret_sauce').should('have.value', 'secret_sauce');
     cy.get('input[data-test="login-button"]').click();
     cy.url().should('eq', 'https://www.saucedemo.com/');
-    cy.get('.error-message-container', {timeout: 10000}).should('be.visible');
+    cy.get('.error-message-container').should('be.visible');
     cy.sauceVisualCheck('Locked User Error Message', {
       captureDom: true
     });
   })
+
+  function openSauceDemoPage() {
+    cy.visit('https://www.saucedemo.com/');
+    // service workers can break Cypress (https://github.com/cypress-io/cypress/issues/16192)
+    cy.intercept('/service-worker.js', {body: undefined});
+  }
 
 });
