a29941d43bd0d78a30cb1ac14c13e00aaa04fe89
Kerem <github@beygo.info>
2024-03-18 15:49:30 +0100

    fix test names

--- cypress/cypress/e2e/saucedemo.cy.ts
+++ cypress/cypress/e2e/saucedemo.cy.ts
@@ -1,6 +1,6 @@
 describe('Sauce Visual Demo', () => {
 
-  it(' - should be able to open the login page', () => {
+  it('should be able to open the login page', () => {
     openSauceDemoPage();
     cy.get('.login_container').should('be.visible');
     cy.sauceVisualCheck('Before Login', {
@@ -8,7 +8,7 @@ describe('Sauce Visual Demo', () => {
     });
   })
 
-  it(` - should be able to login with a standard user`, () => {
+  it(`should be able to login with a standard user`, () => {
     openSauceDemoPage();
     const USER = Cypress.env('VISUAL_CHECK') ? 'visual_user' : 'standard_user';
     cy.get('[data-test="username"]').type(USER).should('have.value', USER);
@@ -20,7 +20,7 @@ describe('Sauce Visual Demo', () => {
     });
   })
 
-  it(' - should not be able to login with a locked user', () => {
+  it('should not be able to login with a locked user', () => {
     openSauceDemoPage();
     cy.get('[data-test="username"]').type('locked_out_user').should('have.value', 'locked_out_user');
     cy.get('[data-test="password"]').type('secret_sauce').should('have.value', 'secret_sauce');
