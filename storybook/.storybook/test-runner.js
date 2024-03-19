c432a018ca0275a2fb2a45bf9a2ce0fdf42ca528
Logan Graham <logan.graham@saucelabs.com>
2024-03-19 14:28:08 -0400

    add example skipping all button stories

--- storybook/.storybook/test-runner.js
+++ storybook/.storybook/test-runner.js
@@ -2,4 +2,7 @@ const { postRender } = require('@saucelabs/visual-storybook');
 
 module.exports = {
     postRender,
+    tags: {
+        skip: ['skip-saucelabs'],
+    },
 };
--- storybook/src/stories/Button.stories.ts
+++ storybook/src/stories/Button.stories.ts
@@ -11,7 +11,7 @@ const meta = {
     layout: 'centered',
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ['skip-saucelabs'],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
