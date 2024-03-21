7fc1734052bca62af5f84f5b9bedb581db472971
Logan Graham <logan.graham@saucelabs.com>
2024-03-21 09:13:24 -0400

    update example to use storybook 8

--- storybook/.storybook/test-runner.js
+++ storybook/.storybook/test-runner.js
@@ -1,5 +1,11 @@
-const { postRender } = require('@saucelabs/visual-storybook');
+const { postVisit } = require('@saucelabs/visual-storybook');
 
+// Prefer to use TypeScript? See the advanced configuration docs on Storybook for an easy example:
+// https://storybook.js.org/docs/writing-tests/test-runner#advanced-configuration
+
+/**
+ * @type {import('@storybook/test-runner').TestRunnerConfig}
+ */
 module.exports = {
-    postRender,
+    postVisit,
 };
