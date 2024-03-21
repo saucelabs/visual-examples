8011fd78005ac3ff4185fa1c6550e2aaa99d3fd8
Logan Graham <loganparkergraham@gmail.com>
2024-03-21 10:30:25 -0400

    update example to use storybook 8 (#348)
    Co-authored-by: Logan Graham <logan.graham@saucelabs.com>

--- storybook/src/stories/Header.stories.ts
+++ storybook/src/stories/Header.stories.ts
@@ -1,16 +1,23 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
 
-import { Header } from './Header';
+import { Header } from "./Header";
 
 const meta = {
-  title: 'Example/Header',
+  title: "Example/Header",
   component: Header,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
+  args: {
+    user: undefined,
+    onLogin: fn(),
+    onLogout: fn(),
+    onCreateAccount: fn(),
+  }
 } satisfies Meta<typeof Header>;
 
 export default meta;
@@ -19,7 +26,7 @@ type Story = StoryObj<typeof meta>;
 export const LoggedIn: Story = {
   args: {
     user: {
-      name: 'Jane Doe',
+      name: "Jane Doe",
     },
   },
 };
