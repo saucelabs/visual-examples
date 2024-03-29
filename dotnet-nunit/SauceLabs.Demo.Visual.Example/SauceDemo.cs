b34bafad3a05c4b8d2b6ddc9ec69214bd0afa401
Felix P <felix@saucelabs.com>
2024-03-29 13:36:51 +0100

    Fix changes

--- dotnet-nunit/SauceLabs.Demo.Visual.Example/SauceDemo.cs
+++ dotnet-nunit/SauceLabs.Demo.Visual.Example/SauceDemo.cs
@@ -5,6 +5,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using SauceLabs.Visual;
+using SauceLabs.Visual.Models;
 
 namespace SauceLabs.Demo.Visual.Example;
 
@@ -63,73 +64,7 @@ public class SauceDemo
         await VisualClient.VisualCheck("Inventory Page");
 
         var results = await VisualClient.VisualResults();
-        // Assert.AreEqual(2, results?[DiffStatus.Unapproved]);
-    }
-
-
-    [Test]
-    public async Task SauceDemo_CheckInventory_ShouldAddBackpackToCartBis()
-    {
-        Driver.Navigate().GoToUrl("https://www.saucedemo.com");
-
-        var usernameLocator = By.CssSelector("#user-name");
-        var passwordLocator = By.CssSelector("#password");
-        var submitLocator = By.CssSelector(".btn_action");
-
-        var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(15));
-        wait.Until(drv => drv.FindElement(usernameLocator));
-
-        await VisualClient.VisualCheck("Login Page - bis");
-
-        var usernameElement = Driver.FindElement(usernameLocator);
-        var passwordElement = Driver.FindElement(passwordLocator);
-        var submitElement = Driver.FindElement(submitLocator);
-
-
-        usernameElement.SendKeys(Utils.GetDemoUsername());
-        passwordElement.SendKeys(Utils.GetDemoPassword());
-        submitElement.Click();
-
-        Assert.AreEqual("https://www.saucedemo.com/inventory.html", Driver.Url);
-        var addToBackpack = Driver.FindElement(By.Id("add-to-cart-sauce-labs-backpack"));
-
-        await VisualClient.VisualCheck("Inventory Page - bis");
-
-        var results = await VisualClient.VisualResults();
-        // Assert.AreEqual(2, results?[DiffStatus.Unapproved]);
-    }
-
-
-    [Test]
-    public async Task SauceDemo_CheckInventory_ShouldAddBackpackToCartThird()
-    {
-        Driver.Navigate().GoToUrl("https://www.saucedemo.com");
-
-        var usernameLocator = By.CssSelector("#user-name");
-        var passwordLocator = By.CssSelector("#password");
-        var submitLocator = By.CssSelector(".btn_action");
-
-        var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(15));
-        wait.Until(drv => drv.FindElement(usernameLocator));
-
-        await VisualClient.VisualCheck("Login Page - third");
-
-        var usernameElement = Driver.FindElement(usernameLocator);
-        var passwordElement = Driver.FindElement(passwordLocator);
-        var submitElement = Driver.FindElement(submitLocator);
-
-
-        usernameElement.SendKeys(Utils.GetDemoUsername());
-        passwordElement.SendKeys(Utils.GetDemoPassword());
-        submitElement.Click();
-
-        Assert.AreEqual("https://www.saucedemo.com/inventory.html", Driver.Url);
-        var addToBackpack = Driver.FindElement(By.Id("add-to-cart-sauce-labs-backpack"));
-
-        await VisualClient.VisualCheck("Inventory Page - third");
-
-        var results = await VisualClient.VisualResults();
-        // Assert.AreEqual(2, results?[DiffStatus.Unapproved]);
+        Assert.AreEqual(2, results?[DiffStatus.Unapproved]);
     }
 
     [OneTimeTearDown]
