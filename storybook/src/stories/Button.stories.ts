8d6091441aa561408a24d581b5b29c3e78eebf02
Logan Graham <loganparkergraham@gmail.com>
2025-05-12 09:59:38 -0400

    [Storybook] Upgrade dependencies, add examples for variations (#783)
    * upgrade storybook dependencies, add examples for variations
    
    * add / setup interactions example with newest Storybook integration version
    
    * Importing SauceVisualParams
    
    * Using SAUCE_VISUAL_PROJECT
    
    * Bumping deps
    
    * Bumping more deps
    
    * A few more deps bumped
    
    ---------
    
    Co-authored-by: Logan Graham <logan.graham@saucelabs.com>
    Co-authored-by: Diego Molina <diemol@users.noreply.github.com>
    Co-authored-by: Diego Molina <diemol@gmail.com>

--- storybook/src/stories/Button.stories.ts
+++ storybook/src/stories/Button.stories.ts
@@ -1,17 +1,77 @@
 import type { Meta, StoryObj } from "@storybook/react";
-
+import { takeVisualSnapshot } from "@saucelabs/visual-storybook/play";
+import { SauceVisualParams } from "@saucelabs/visual-storybook";
 import { Button } from "./Button";
+import { expect, userEvent, within } from '@storybook/test';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: "Example/Button",
   component: Button,
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = canvas.getByRole('button');
+    await userEvent.hover(button);
+
+    // Take a screenshot of the focus behavior
+    await button.focus();
+    await expect(button).toHaveFocus();
+    await takeVisualSnapshot('Button Focused');
+
+    await userEvent.click(button);
+    await takeVisualSnapshot('Button Clicked / Active');
+
+    // Unfocus the element to reset it for the remaining screenshots / variations
+    await button.blur();
+  },
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: "centered",
+    sauceVisual: {
+      variations: [
+        {
+          // Prefixes the story name with `[disabled]` -- eg: `Example/Button/[disabled] Button`.
+          prefix: '[disabled] ',
+          args: {
+            disabled: true,
+          },
+        },
+        // Examples to test multiple sizes of a single component
+        {
+          prefix: '[small] ',
+          args: {
+            size: "small",
+          },
+        },
+        {
+          prefix: '[medium] ',
+          args: {
+            size: "medium",
+          },
+        },
+        {
+          prefix: '[large] ',
+          args: {
+            size: "large",
+          },
+        },
+        {
+          // postfixes the story name with ` Primary` -- eg: `Example/Button/Button Primary`.
+          postfix: ' Primary',
+          args: {
+            primary: true,
+          },
+        },
+        {
+          // Overrides the name completely. -- eg `Example/Button/Btn primary=true`
+          name: 'Btn primary=true',
+          args: {
+            primary: true,
+          },
+        },
+      ],
+    } satisfies SauceVisualParams<Meta<typeof Button>['args']>,
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: "color" },
@@ -21,30 +81,8 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary: Story = {
-  args: {
-    primary: true,
-    label: "Button",
-  },
-};
-
-export const Secondary: Story = {
+export const _Button: Story = {
   args: {
     label: "Button",
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: "large",
-    label: "Button!",
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: "small",
-    label: "Button!",
   },
 };
