13db94cc98903e1b467c92cd2b8687682f3c8ed6
Logan Graham <logan.graham@saucelabs.com>
2026-02-26 12:37:17 -0500

    update runner config

--- storybook/.storybook/test-runner.ts
+++ storybook/.storybook/test-runner.ts
@@ -1,7 +1,7 @@
-// .storybook/test-runner.ts
+import { TestRunnerConfig } from '@storybook/test-runner';
 import { postVisit, preVisit  } from "@saucelabs/visual-storybook";
 
 export default {
     preVisit,
     postVisit,
-};
+} satisfies TestRunnerConfig;
